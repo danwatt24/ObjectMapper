9ad54a8f0561869b54f1991f2eb4bb9da471d7d0
Dan W <danwatt24@gmail.com>
2020-04-17 19:32:55 -0700

    Renamed the nested classes, relevant properties, and variables.

--- ObjectMapperTests/PrimitiveTests.cs
+++ ObjectMapperTests/PrimitiveTests.cs
@@ -16,93 +16,93 @@ namespace ObjectMapperTests
         [Test]
         public void NullSource()
         {
-            var simpleB = _mapper.map<SimpleB>(null);
-            Assert.IsNull(simpleB);
+            var primitiveB = _mapper.map<PrimitiveB>(null);
+            Assert.IsNull(primitiveB);
         }
 
         [Test]
         public void ReturnsBasicObject()
         {
-            var simpleA = new SimpleA();
-            var simpleB = _mapper.map<SimpleB>(simpleA);
-            Assert.IsNotNull(simpleB);
-            Assert.AreEqual(simpleB.GetType(), typeof(SimpleB));
+            var primitiveA = new PrimitiveA();
+            var primitiveB = _mapper.map<PrimitiveB>(primitiveA);
+            Assert.IsNotNull(primitiveB);
+            Assert.AreEqual(primitiveB.GetType(), typeof(PrimitiveB));
         }
 
         [Test]
         public void WithValue()
         {
-            var simpleA = new SimpleA { Simple = 1 };
-            var simpleB = _mapper.map<SimpleB>(simpleA);
-            Assert.AreEqual(simpleA.Simple, simpleB.Simple);
+            var primitiveA = new PrimitiveA { Primitive = 1 };
+            var primitiveB = _mapper.map<PrimitiveB>(primitiveA);
+            Assert.AreEqual(primitiveA.Primitive, primitiveB.Primitive);
 
-            simpleA.Simple = 13;
-            var simpleC = _mapper.map<SimpleB>(simpleA);
-            Assert.AreEqual(simpleA.Simple, simpleC.Simple);
-            Assert.AreNotEqual(simpleB.Simple, simpleC.Simple);
+            primitiveA.Primitive = 13;
+            var primitiveC = _mapper.map<PrimitiveB>(primitiveA);
+            Assert.AreEqual(primitiveA.Primitive, primitiveC.Primitive);
+            Assert.AreNotEqual(primitiveB.Primitive, primitiveC.Primitive);
         }
 
         [Test]
         public void MissingDestProp()
         {
-            var simpleC = new SimpleC();
-            var simpleA = _mapper.map<SimpleA>(simpleC);
-            Assert.AreEqual(simpleC.Simple, simpleA.Simple);
+            var primitiveC = new PrimitiveC();
+            var primitiveA = _mapper.map<PrimitiveA>(primitiveC);
+            Assert.AreEqual(primitiveC.Primitive, primitiveA.Primitive);
         }
 
         [Test]
         public void MissingDestPropWithValues()
         {
-            var simpleC = new SimpleC { Simple = 32, HereOnly = 12 };
-            var simpleA = _mapper.map<SimpleA>(simpleC);
-            Assert.AreEqual(simpleC.Simple, simpleA.Simple);
+            var primitiveC = new PrimitiveC { Primitive = 32, HereOnly = 12 };
+            var primitiveA = _mapper.map<PrimitiveA>(primitiveC);
+            Assert.AreEqual(primitiveC.Primitive, primitiveA.Primitive);
         }
 
         [Test]
         public void SkipsUnassignable()
         {
-            var simpleD = new SimpleD { Simple = "blah" };
-            var simpleA = _mapper.map<SimpleA>(simpleD);
-            Assert.NotNull(simpleA);
+            var primitiveD = new PrimitiveD { Primitive = "blah" };
+            var primitiveA = _mapper.map<PrimitiveA>(primitiveD);
+            Assert.NotNull(primitiveA);
         }
 
         [Test]
         public void PropMismatch()
         {
-            var hasProps = new HasMoreProps { Blah = "blah", Meh = 10f, Simple = 12, SomeOther = "other", YetAnother = "yet" };
-            var simpleA = _mapper.map<SimpleA>(hasProps);
-            Assert.AreEqual(hasProps.Simple, simpleA.Simple);
+            var hasProps = new HasMoreProps { Blah = "blah", Meh = 10f, Primitive = 12, SomeOther = "other", YetAnother = "yet" };
+            var primitiveA = _mapper.map<PrimitiveA>(hasProps);
+            Assert.AreEqual(hasProps.Primitive, primitiveA.Primitive);
 
-            simpleA = new SimpleA { Simple = 76 };
-            hasProps = _mapper.map<HasMoreProps>(simpleA);
-            Assert.AreEqual(simpleA.Simple, hasProps.Simple);
+            primitiveA = new PrimitiveA { Primitive = 76 };
+            hasProps = _mapper.map<HasMoreProps>(primitiveA);
+            Assert.AreEqual(primitiveA.Primitive, hasProps.Primitive);
             Assert.AreEqual(default(float), hasProps.Meh);
             Assert.AreEqual(default(string), hasProps.Blah);
         }
 
-        private class SimpleA
+        private class PrimitiveA
         {
-            public int Simple { get; set; }
+            public int Primitive { get; set; }
         }
 
-        private class SimpleB
+        private class PrimitiveB
         {
-            public int Simple { get; set; }
+            public int Primitive { get; set; }
         }
 
-        private class SimpleC : SimpleA
+        private class PrimitiveC : PrimitiveA
         {
             public int HereOnly { get; set; }
         }
 
-        private class SimpleD
+        private class PrimitiveD
         {
-            public string Simple { get; set; }
+            public string Primitive { get; set; }
         }
 
         private class HasMoreProps
         {
-            public int Simple { get; set; }
+            public int Primitive { get; set; }
             public string Blah { get; set; }
             public float Meh { get; set; }
             public string SomeOther { get; set; }
