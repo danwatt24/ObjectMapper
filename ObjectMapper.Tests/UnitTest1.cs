eeabd8eee1973b2ca6b9f8fd23e37ad2a9cb10df
Dan <danwatt24@gmail.com>
2022-11-13 14:40:28 -0800

    Revamp of the project to target net6

---
+++ ObjectMapper.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ObjectMapper.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- ObjectMapper/ObjectMapper.cs
+++ ObjectMapper/ObjectMapper.cs
@@ -1,86 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
+﻿using System.Reflection;
 
-/*
- * TODO LIST:
- *      provide methods to "pre-register" mapping types
- *      create memoized version of property getters and setters
- *          to avoid having to use reflection every time. (this dependent on the previous item)
- *      figure out how to return a builder object to allow for custom mappings (CustomMappingTests)
- *      create benchmark "tests" to compare to automapper
-*/
 namespace ObjectMapper
 {
-    public class ObjectMapper : IObjectMapper
+    public class Mapper
     {
-        private static readonly Dictionary<Type, Func<object>> Creators;
-        static ObjectMapper()
+        private static readonly Dictionary<Type, Dictionary<string, PropertyInfo>> PROPS;
+        static Mapper()
         {
-            Creators = new Dictionary<Type, Func<object>>();
+            PROPS = new Dictionary<Type, Dictionary<string, PropertyInfo>>();
         }
 
-        public Dest Map<Dest>(object source) => (Dest)Map(source, typeof(Dest));
-
-        public object Map(object source, Type mapTo)
+        public static To Map<From, To>(From a)
         {
-            if (source == null) return null;
+            var fromProps = getProps<From>();
+            var toProps = getProps<To>();
+            var result = Activator.CreateInstance<To>();
 
-            var dest = GetCreator(mapTo)();
-            var sourceProps = source.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            var destProps = dest.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var sourceProp in sourceProps)
+            foreach (var toProp in toProps)
             {
-                var value = sourceProp.GetValue(source);
-                var destProp = destProps.FirstOrDefault(p => p.Name == sourceProp.Name);
-                if (destProp != null)
-                {
-                    var sourceType = sourceProp.PropertyType;
-                    var destType = destProp.PropertyType;
-                    if (destType.IsAssignableFrom(sourceType))
-                    {
-                        if (sourceType.IsArray)
-                            value = ArrayCopy(value);
-
-                        destProp.SetValue(dest, value);
-                    }
-                }
+                if (!fromProps.TryGetValue(toProp.Key, out var from)) continue;
+                toProp.Value.SetValue(result, from.GetValue(a));
             }
 
-            return dest;
+            return result;
         }
 
-        private object ArrayCopy(object arr) => ArrayCopy((Array)arr);
-        private Array ArrayCopy(Array arr)
+        public static void Register<T>() => Register(typeof(T));
+        public static void Register(Type t) => saveProps(t);
+
+        private static Dictionary<string, PropertyInfo> getProps<T>() => getProps(typeof(T));
+        private static Dictionary<string, PropertyInfo> getProps(Type t)
         {
-            var type = arr.GetType();
-            var elementType = type.GetElementType();
-            var nArr = Array.CreateInstance(type.GetElementType(), arr.Length);
-            if (elementType.IsPrimitive)
-                Array.Copy(arr, nArr, arr.Length);
-            else
+            saveProps(t);
+            return PROPS[t];
+        }
+
+        private static void saveProps(Type t)
+        {
+            if (!PROPS.ContainsKey(t))
             {
-                for (var i = 0; i < nArr.Length; i++)
-                {
-                    var value = arr.GetValue(i);
-                    var nValue = Map(value, elementType);
-                    nArr.SetValue(nValue, i);
-                }
+                var props = t.GetProperties().ToDictionary(p => p.Name, p => p);
+                PROPS.Add(t, props);
             }
-            return nArr;
-        }
-
-        private Func<object> GetCreator(Type t)
-        {
-            if (!Creators.ContainsKey(t))
-                // https://stackoverflow.com/a/29972767/6038906
-                Creators.Add(t, Expression.Lambda<Func<object>>(
-                    Expression.New(t.GetConstructor(Type.EmptyTypes))
-                ).Compile());
-
-            return Creators[t];
         }
     }
 }
